cc9e02741b39f50523b561a59531c71ab1dcea7c
Utkarsh <gutkarsh20@iitk.ac.in>
2022-02-15 22:16:03 +0530

    few bugs fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,87 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.TreeSet;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     ArrayList<Song> songList = new ArrayList<Song>();
 
-    public static void main(String[] args) {
-        new Main().go();
+    public static void main(String[] args) throws IOException {
+        Main obj=new Main();
+        obj.getSongs();
+        obj.go(0);
     }
 
-    public void go() {
-        getSongs();
-        System.out.println("Now Playing - "+songList.get(0));
+    public void go(int ind) throws IOException {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("Now Playing - "+songList.get(ind));
         System.out.println("Enter the corresponding numbers to perform functions -");
         System.out.println("1. Next Song");
         System.out.println("2. Previous Song");
-        System.out.println("3. Replay Current Song");
-        System.out.println("4. List all songs");
-        System.out.println("5. Delete Current Song");
-        System.out.println("6. Add a new song");
-        System.out.println("7. Shuffle Play");
+        System.out.println("3. List all songs");
+        System.out.println("4. Delete Current Song");
+        System.out.println("5. Add a new song");
+        System.out.println("6. Shuffle Play");
         System.out.println("0. Quit Jukebox");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ch = Integer.parseInt(br.readLine());
+        switch(ch) {
+            case 1:
+                if(ind<songList.size()-1)
+                    ind++;
+                else
+                    System.out.println("Queue Limit reached.");
+                System.out.println(songList.size());
+                break;
+            case 2:
+                if(ind>0)
+                    ind--;
+                else
+                    System.out.println("No previous track available");
+                break;
 
+            case 3:
+                System.out.println(songList);
+                break;
+
+            case 4:
+                songList.remove(ind);
+                if(ind == songList.size()-1)
+                    ind--;
+                break;
+
+            case 5:
+                System.out.print("Song Title: ");
+                String title = br.readLine();
+                System.out.println();
+                System.out.print("Artist Name: ");
+                String artist = br.readLine();
+                System.out.println();
+                System.out.print("Song Rating: ");
+                String rating = br.readLine();
+                System.out.println();
+                System.out.print("Song BPM: ");
+                String bpm = br.readLine();
+                System.out.println();
+                String str = title+"/"+artist+"/"+rating+"/"+bpm;
+                addSong(str);
+                break;
+
+            case 6:
+                Collections.shuffle(songList);
+                break;
+
+            case 0:
+                System.out.println("Thank you for your time");
+                System.exit(0);
+                break;
+
+            default:
+                System.out.println("Enter a valid choice");
+            }
+        go(ind);
 
         //System.out.println(songList);
         //Collections.sort(songList);
